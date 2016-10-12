0de7bdd463439aaa8104fe5c9f89d0a61bdf5ca7
dsun092 <dsun092@gmail.com>
2016-10-11 21:29:39 -0700

    Updates to binary tree

--- binarytree.py
+++ binarytree.py
@@ -1,141 +1,198 @@
 class Node:
-    def __init__(self, val):
+    '''
+    Node class has the following properties:
+        * key, val pair where key is comparable between nodes
+        * left  child
+        * right child
+        + compare: compares current node key with input key. Return -1, 0, or 1
+    '''
+    def __init__(self, key, val):
+        self.key = key
         self.val = val
-        self.leftChild = None
-        self.rightChild = None
-        self.balance = 0
-        self.parent = None
-    def getLeftChild(self):
-        return self.leftChild
-    def getRightChild(self):
-        return self.rightChild
-    def getParent(self):
-        return self.parent
-    def getVal(self):
-        return self.val
+        self.left = None
+        self.right = None
+        self.count = 1
+
+    def compare(self, key):
+        '''
+        Return -1 if self key is less than input key
+        Return 1 if self key is greater than input key
+        Return 0 if self key is equals to input key
+        '''
+        if self.key < key:
+            return 1
+        elif self.key > key:
+            return -1
+        else:
+            return 0
 
 class BinaryTree:
     def __init__(self):
         self.root = None
-        self.size = 0
-    def length(self):
-        return self.size
 
-    def insert(self, val):
-        temp = Node(val)
-        if self.root is None:
-            self.root = temp
-        else:
-            current = self.root
-            parent = None
-            found = False
-            while not found:
-                if val < current.getVal():
-                    if current.getLeftChild():
-                        parent = current
-                        current = current.getLeftChild()
-                    else:
-                        current.leftChild = temp
-                        temp.parent = current
-                        self.updateBalance(current.leftChild)
-                        found = True
-                else:
-                    if current.getRightChild():
-                        parent = current
-                        current = current.getRightChild()
-                    else:
-                        current.rightChild = temp
-                        temp.parent = current
-                        self.updateBalance(current.rightChild)
-                        found = True
-    def updateBalance(self, node):
-        if node.balance > 1 or node.balance < -1:
-            self.rebalance(node)
-        else:
-            if node.parent is not None:
-                if node.parent.leftChild == node:
-                    node.parent.balance = node.parent.balance + 1
-                elif node.parent.rightChild == node:
-                    node.parent.balance = node.parent.balance - 1
-                if node.parent.balance != 0:
-                    self.updateBalance(node.parent)
-
-    def rebalance(self, node):
-        if node.balance < 0:
-            if node.rightChild.balance > 0:
-                self.rotateRight(node.rightChild)
-                self.rotateLeft(node)
+    def get(self, key):
+        '''
+        Returns the value of node with input key match
+        If there are no match, return None
+        '''
+        node = self.root
+        while node != None:
+            comp = node.compare(key)
+            if comp < 0:
+                node = node.left
+            elif comp > 0:
+                node = node.right
             else:
-                self.rotateLeft(node)
-        if node.balance > 0:
-            if node.leftChild.balance < 0:
-                self.rotateLeft(node.leftChild)
-                self.rotateRight(node)
-            else:
-                self.rotateRight(node)
+                return node.val
+        return None
 
-    def rotateLeft(self, rotRoot):
-        newRoot = rotRoot.rightChild
-        rotRoot.rightChild = newRoot.leftChild
-        if newRoot.leftChild != None:
-            newRoot.leftChild.parent = rotRoot
-        if rotRoot.parent is None:
-            self.root = newRoot
-        else:
-            if rotRoot.parent.leftChild == rotRoot:
-                rotRoot.parent.leftChild = newRoot
-            elif rotRoot.parent.rightChild == rotRoot:
-                rotRoot.parent.rightChild = newRoot
-        newRoot.parent = rotRoot.parent
-        newRoot.leftChild = rotRoot
-        rotRoot.parent = newRoot
-        rotRoot.balance = rotRoot.balance + 1 - min(newRoot.balance, 0)
-        newRoot.balance = newRoot.balance + 1 + max(rotRoot.balance, 0)
+    def put(self, key, val):
+        '''
+        Inserts key, val pair into tree
+        '''
+        self.root = self._put(self.root, key, val)
 
-    def rotateRight(self, rotRoot):
-        newRoot = rotRoot.leftChild
-        rotRoot.leftChild = newRoot.rightChild
-        if newRoot.rightChild != None:
-            newRoot.rightChild.parent = rotRoot
-        if rotRoot.parent is None:
-            self.root = newRoot
+    def _put(self, node, key, val):
+        '''
+        Recursive function to put key, val Node into tree.
+        '''
+        if node is None:
+            return Node(key, val)
+        comp = node.compare(key)
+        if comp < 0:
+            node.left = self._put(node.left, key, val)
+        elif comp > 0:
+            node.right = self._put(node.right, key, val)
         else:
-            if rotRoot.parent.leftChild == rotRoot:
-                rotRoot.parent.leftChild = newRoot
-            elif rotRoot.parent.rightChild == rotRoot:
-                rotRoot.parent.rightChild = newRoot
-        newRoot.parent = rotRoot.parent
-        rotRoot.parent = newRoot
-        newRoot.rightChild = rotRoot
-        rotRoot.balance = rotRoot.balance - 1 - max(newRoot.balance, 0)
-        newRoot.balance = newRoot.balance - 1 + min(rotRoot.balance, 0)
+            node.val = val
+        node.count = 1 + self._size(node.left) + self._size(node.right)
+        return node
 
-    def search(self, val):
-        if self.root is None:
-            return False
+    def minVal(self):
+        '''
+        returns the value of the smallest key in tree
+        '''
+        node = self.root
+        while node.left is not None:
+            node = node.left
+        return node.val
+
+    def maxVal(self):
+        '''
+        returns the value of the largest key in tree
+        '''
+        node = self.root
+        while node.right is not None:
+            node = node.right
+        return node.val
+
+    def floor(self, key):
+        '''
+        Returns the largest value where key is <= input key
+        '''
+        node = self._floor(self.root, key)
+        if node is None:
+            return None
+        return node.val
+
+    def _floor(self, node, key):
+        '''
+        If node is none, return none
+        If input key is less than key at root, we go to left subtree
+        If input key is greater than key at root, floor of k is in
+            the right subtree
+        '''
+        if node is None:
+            return None
+        comp = node.compare(key)
+        if comp == 0:
+            return node
+        elif comp < 0:
+            return self._floor(node.left, key)
+        subroot = self._floor(node.right, key)
+        if subroot is not None:
+            return subroot
         else:
-            current = self.root
-            while current is not None:
-                if current.getVal() == val:
-                    return True
-                elif val < current.getVal():
-                    if current.leftChild:
-                        current = current.getLeftChild()
-                    else:
-                        return False
-                elif val > current.getVal():
-                    if current.rightChild:
-                        current = current.getRightChild()
-                    else:
-                        return False
-        return False
-    def getMax(self):
-        cur = self.root
-        while cur.rightChild is not None:
-            cur = cur.rightChild
-        return cur.getVal()
-    def getMin(self):
-        cur = self.root
-        while cur.leftChild is not None:
-            cur = cur.leftChild
-        return cur.getVal()
+            return node
+
+    def ceiling(self, key):
+        '''
+        Returns the largest value where key is >= input key
+        '''
+        node = self._ceiling(self.root, key)
+        if node is None:
+            return None
+        return node.val
+
+    def _ceiling(self, node, key):
+        if node is None:
+            return None
+        comp = node.compare(key)
+        if comp == 0:
+            return node
+        elif comp > 0:
+            return self._ceiling(node.right, key)
+        subtree = self._ceiling(node.left, key)
+        if subtree is not None:
+            return subtree
+        else:
+            return node
+    def size(self):
+        '''
+        Returns the size of tree
+        '''
+        return self._size(self.root)
+    def _size(self, node):
+        if node is None:
+            return 0
+        else:
+            return node.count
+
+    def rank(self, key):
+        '''
+        Returns # of keys in tree that are smaller than input key
+        '''
+        return self._rank(self.root, key)
+
+    def _rank(self, node, key):
+        '''
+        If node is none return 0
+        If node is less than input key, then rank is rank of node's left child
+        if node is greater than input key, then rank is
+            number of nodes in left subtree + current node + rank of right subtree
+        '''
+        if node is None:
+            return 0
+        comp = node.compare(key)
+        if comp < 0:
+            return self._rank(node.left, key)
+        elif comp > 0:
+            return 1 + self._size(node.left) + self._rank(node.right, key)
+        else:
+            return self._size(node.left)
+
+    def inordertraversal(self, node):
+        '''
+        Prints out the values within the tree in ascending order according to
+        the keys of the tree.
+        '''
+        if node is None:
+            return
+        self.inordertraversal(node.left)
+        print node.val
+        self.inordertraversal(node.right)
+
+    def reversetraversal(self, node):
+        if node is None:
+            return
+        self.reversetraversal(self, node.right)
+        print node.val
+        self.reversetraversal(self, node.left)
+
+def constructtree():
+    tree = BinaryTree()
+    input_nodes = [{50: 'A'}, {10: 'B'}, {5: 'C'}, {17: 'T'}, {84: 'N'}, {56: 'K'}, {90: 'QQ'}, {9: 'Q'}]
+    for item in input_nodes:
+        for k,v in item.iteritems():
+            tree.put(k,v)
+    return tree
--- cracking/chap3.py
+++ cracking/chap3.py
@@ -59,3 +59,33 @@ class QueueS:
     def swap_stack(self):
         while self.current_stack != []:
             self.pop_stack.append(self.current_stack.pop())
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, val):
+        self.stack.append(val)
+    def pop(self):
+        return self.stack.pop()
+    def peek(self):
+        return self.stack[-1]
+
+
+def sortStack(s):
+    sorted_stack = Stack()
+    while s.stack != []:
+        temp = s.pop()
+        found = False
+        while not found:
+            if sorted_stack.stack == []:
+                found = True
+            elif sorted_stack.peek() < temp:
+                found = True
+            else:
+                s.push(sorted_stack.pop())
+        sorted_stack.push(temp)
+        while s.stack != [] and s.peek() > sorted_stack.peek():
+            sorted_stack.push(s.pop())
+    return sorted_stack
+
+
